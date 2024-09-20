32ea93434456f01e814bc9aa3b9f2dbd35dd816f
lucas <lucasgheno600@gmail.com>
2024-09-19 21:15:14 -0300

    chore: created About project

---
+++ src/app/sobre/page.tsx
@@ -0,0 +1,21 @@
+export default function About() {
+    return (
+      <main className="p-8">
+        <h1 className="text-3xl font-bold mb-4">Sobre o Gerenciador de Orçamento Pessoal do Ghenoninho 🦧</h1>
+        <p className="mb-4">
+          Este site foi criado como um projeto na faculdade.
+        </p>
+        <h2 className="text-2xl font-bold mb-2">Stacks utilizadas:</h2>
+        <ul className="list-disc list-inside mb-4">
+          <li>Next.js</li>
+          <li>React</li>
+          <li>TypeScript</li>
+          <li>Tailwind CSS</li>
+          <li>Context API para gerenciamento de estado</li>
+          <li>LocalStorage para armazenamento de dados local</li>
+          <li>React-icons para icons</li>
+
+        </ul>
+      </main>
+    )
+  }
