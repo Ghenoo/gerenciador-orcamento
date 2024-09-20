d87fd16e6501ecdd1fc0dbaf00f4971c29b320cc
Ghenoo <lucasgheno600@gmail.com>
2024-09-20 00:57:20 -0300

    refactor: changed color nav and flex space

--- src/components/Navigation.tsx
+++ src/components/Navigation.tsx
@@ -2,11 +2,11 @@ import Link from 'next/link'
 
 export default function Navigation() {
   return (
-    <nav className="bg-gray-800 text-white p-4">
-      <ul className="flex space-x-4">
-        <li><Link href="/" className="hover:text-gray-300">Home</Link></li>
-        <li><Link href="/dashboard" className="hover:text-gray-300">Dashboard</Link></li>
-        <li><Link href="/sobre" className="hover:text-gray-300">Sobre</Link></li>
+    <nav className="bg-gray-500 text-white p-4">
+      <ul className="flex space-x-5">
+        <li><Link href="/" className="font-bold hover:text-gray-300">Home</Link></li>
+        <li><Link href="/dashboard" className="font-bold hover:text-gray-300">Dashboard</Link></li>
+        <li><Link href="/sobre" className="font-bold hover:text-gray-300">Sobre</Link></li>
       </ul>
     </nav>
   )
