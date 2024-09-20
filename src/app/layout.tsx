cdce80fca6c0910666787a2bf004c46f659e3c9c
Ghenoo <lucasgheno600@gmail.com>
2024-09-20 01:53:40 -0300

    fix: the bug in deploy

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -1,7 +1,6 @@
 import './globals.css'
 import type { Metadata } from 'next'
 import { Inter } from 'next/font/google'
-import { BudgetProvider } from '@/contexts/BudgetContext'
 import Navigation from '@/components/Navigation'
 import dynamic from 'next/dynamic'
 
