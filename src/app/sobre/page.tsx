185de66c4d830fa79c2f4602e86dc06d5c6cbcdb
Ghenoo <lucasgheno600@gmail.com>
2024-09-20 00:56:07 -0300

    refactor: text changed

--- src/app/sobre/page.tsx
+++ src/app/sobre/page.tsx
@@ -3,7 +3,7 @@ export default function About() {
       <main className="p-8">
         <h1 className="text-3xl font-bold mb-4">Sobre o Gerenciador de Orçamento Pessoal do Ghenoninho 🦧</h1>
         <p className="mb-4">
-          Este site foi criado como um projeto na faculdade.
+          Este site foi criado como um projeto para uma matéria da faculdade.
         </p>
         <h2 className="text-2xl font-bold mb-2">Stacks utilizadas:</h2>
         <ul className="list-disc list-inside mb-4">
